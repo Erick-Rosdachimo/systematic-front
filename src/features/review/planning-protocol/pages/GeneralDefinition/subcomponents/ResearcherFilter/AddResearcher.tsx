bb719e2520315b3574d0dd7f9b3836471eb9761d
pedrozzneo <pedrozzneo@gmail.com>
2026-05-05 23:58:28 -0300

    refactor: simplify the flow of filtering the researchers

--- src/features/review/planning-protocol/pages/GeneralDefinition/subcomponents/ResearcherFilter/AddResearcher.tsx
+++ src/features/review/planning-protocol/pages/GeneralDefinition/subcomponents/ResearcherFilter/AddResearcher.tsx
@@ -3,87 +3,106 @@ import { useState, useEffect } from "react";
 import EventButton from "@components/common/buttons/EventButton";
 import { researchersMock } from "../../../../../../../mocks/researchers";
 
-export default function AddResearcher() {
-  const [inputText, setInputText] = useState("");
+export default function AddResearcher({researchers, setResearchers}:any) {
+  const [search, setSearch] = useState("");
   const [potentialResearchers, setPotentialResearchers] = useState(researchersMock.filter((researcher) => researcher.status == "none"));
-  const [selectedPotentialResearchers, setSelectedPotentialResearchers] = useState(researchersMock.filter((researcher) => researcher.status == "none").slice(0, 3));
   const [suggestionsOpen, setSuggestionsOpen] = useState(false);
   const [researcherChosen, setResearcherChosen] = useState(false);
   const [researcherChosenId, setResearcherChosenId] = useState("");
 
   useEffect(() => {
     // simulating API call to get potential researchers
-    setPotentialResearchers(researchersMock.filter((researcher) => researcher.status == "none"));
+    setPotentialResearchers(researchers);
   }, []);
 
-  const filterResearchers = (search: string) => {
-    return potentialResearchers.filter((researcher) => {
-      const fullResearcherReference = `${researcher.name} - ${researcher.email}`;
-      return (fullResearcherReference.toLowerCase().includes(search.toLowerCase()));
-    });
-  };
-
-  const handleFilterResearchers = (search: string) => {
-    const filteredResearchers = filterResearchers(search);
-    setSelectedPotentialResearchers(filteredResearchers.slice(0, 3));
-  };
-
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputText(e.target.value);
+    setSearch(e.target.value);
     setResearcherChosenId("");
     setResearcherChosen(false);
     setSuggestionsOpen(true);
-    handleFilterResearchers(e.target.value);
   };
 
   const handleAddResearcher = () => {
-    //send researcher to server
-    console.log("Adding researcher:", inputText);
-
     // Remove the new pending researcher from the potential researchers list
-    setSelectedPotentialResearchers(potentialResearchers.filter((researcher) => researcher.status == "none" && researcher.id !== researcherChosenId).slice(0, 3));
-
-    // Add the new pending researcher to the added researchers list (need to get it from props?)
+    setPotentialResearchers(potentialResearchers.filter((researcher) => {
+      const fullResearcherReference = `${researcher.name} - ${researcher.email}`;
+      if(fullResearcherReference == search){
+        console.log(fullResearcherReference + " is equal to " + search);
+        return false;
+      }
+      else{
+        console.log(fullResearcherReference + " is different from " + search);
+        return true;
+      }
+    }))
 
     // Reset all states
     setResearcherChosen(false);
-    setInputText("");
+    setSearch("");
     setSuggestionsOpen(false);
   };
+
+  function filterSearch(search: string){
+    return function (researcher:any){
+      const fullResearcherReference = `${researcher.name} - ${researcher.email}`;
+      return (fullResearcherReference.toLowerCase().includes(search.toLowerCase()));
+    }
+  }
+
+  function filterStatus(research:any){
+    return (research.status == "none")
+  }
 
   return (
     <Flex justify="center" py={2}>
       <Flex gap={2} align="center" width="28rem" position="relative">
-        <Input style = {{ backgroundColor: researcherChosen ? "#C9D9E5" : "#ffffffff" }} value={inputText} placeholder="Add a researcher" flex="1" minW={0} size="md" onChange = {handleInputChange} onFocus={() => setSuggestionsOpen(true)} onBlur={() => setSuggestionsOpen(false)}/>
+
+        {/* {researchers
+          .filter(filterSearch("a"))
+          .map((researcher:any) => {
+          return <Text> {researcher.name}</Text>
+        })} */}
+
+        <Input 
+          style = {{ backgroundColor: researcherChosen ? "#C9D9E5" : "#ffffffff" }} flex="1" minW={0} size="md"  
+          value={search} 
+          placeholder="Add a researcher" 
+          onChange = {handleInputChange} 
+          onFocus={() => setSuggestionsOpen(true)}
+          onBlur={() => setSuggestionsOpen(false)}
+        />
         
         {suggestionsOpen && (
             <Box position="absolute" width="25rem" top="100%" mt={1} bg="white" border="1px solid" borderColor="gray.300" borderRadius="md">
-              {selectedPotentialResearchers.length > 0 ? (
-                selectedPotentialResearchers.map((researcher) => (
-                  <Flex
-                    key={researcher.id}
-                    align="center"
-                    gap={3}
-                    px={3}
-                    py={2}
-                    cursor="pointer"
-                    _hover={{ bg: "gray.100" }}
-                    onMouseDown={(e) => e.preventDefault()}
-                    onClick={() => {
-                      setInputText(`${researcher.name} - ${researcher.email}`);
-                      setResearcherChosen(true);
-                      setResearcherChosenId(researcher.id);
-                      handleFilterResearchers(`${researcher.name} - ${researcher.email}`);
-                      setSuggestionsOpen(false);
-                    }}
-                  >
-                    <Avatar size="sm" name="Potential Researcher 1" />
-                    <Text flex="1" fontSize="sm">{researcher.name} - {researcher.email}</Text>
-                  </Flex>
-                ))
-              ) : (
-                <Text color="gray.500" textAlign="center">No researchers found</Text>
-              )
+              {researchers.length > 0 ? (
+                researchers
+                  .filter(filterStatus)
+                  .filter(filterSearch(search))
+                  .slice(0,3)
+                  .map((researcher:any) => (
+                    <Flex
+                      key={researcher.id}
+                      align="center"
+                      gap={3}
+                      px={3}
+                      py={2}
+                      cursor="pointer"
+                      _hover={{ bg: "gray.100" }}
+                      onMouseDown={(e) => e.preventDefault()}
+                      onClick={() => {
+                        setSearch(`${researcher.name} - ${researcher.email}`);
+                        setResearcherChosen(true);
+                        setResearcherChosenId(researcher.id);
+                        setSuggestionsOpen(false);
+                      }}
+                    >
+                      <Avatar size="sm" name="Potential Researcher 1" />
+                      <Text flex="1" fontSize="sm">{researcher.name} - {researcher.email}</Text>
+                    </Flex>
+                  ))
+                ) : (
+                  <Text color="gray.500" textAlign="center">No researchers found</Text>
+                )
             }
         </Box>
         )}
--- src/features/review/planning-protocol/pages/GeneralDefinition/subcomponents/ResearcherFilter/index.tsx
+++ src/features/review/planning-protocol/pages/GeneralDefinition/subcomponents/ResearcherFilter/index.tsx
@@ -1,14 +1,18 @@
 import { Text, VStack } from "@chakra-ui/react";
 import AddResearcher from "./AddResearcher";
 import IncludedResearchers from "./IncludedResearchers";
+import {useState} from 'react';
+import { researchersMock } from "../../../../../../../mocks/researchers";
 
 export default function ResearcherFilter() {
+  const [researchers, setResearchers] = useState(researchersMock)
+
   return (
     <>
       <Text mt={"30px"} fontWeight={500} fontSize={"large"}>Researchers</Text>
 
       <VStack spacing={0} align="stretch" border="2px solid" borderColor="gray.300" borderRadius="md" bgColor="#ffffffff" px={2} py={2}>
-        <AddResearcher />
+        <AddResearcher researchers={researchers} setResearchers={setResearchers}/>
         <IncludedResearchers />
       </VStack>
     </>
