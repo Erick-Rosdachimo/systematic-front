eacf655fd38934dd854ec08879c8b25ad0d05507
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-05-18 13:26:23 -0300

    feat: add filtering options for Graphics page to the ColumnVisibilityMenu component

--- src/features/review/shared/components/common/menu/ColumnVisibilityMenu/index.tsx
+++ src/features/review/shared/components/common/menu/ColumnVisibilityMenu/index.tsx
@@ -17,7 +17,7 @@ type ColumnVisibilityMenuInput = {
 import styles from "./styles.module.css";
 
 const columnLabels: {
-  [K in keyof Omit<ColumnVisibility, "studyReviewId">]: string;
+  [K in keyof Omit<ColumnVisibility, "studyReviewId" | "source" | "answer">]: string;
 } = {
   title: "Title",
   authors: "Author",
@@ -27,6 +27,16 @@ const columnLabels: {
   extractionStatus: "Extraction",
   score: "Score",
   readingPriority: "Priority",
+  included: "Included",
+  excluded: "Excluded",
+  total: "Total",
+  indexingRate: "IndexingRate",
+  precisionRate: "PrecisionRate",
+  sources: "Sources",
+  ic: "IC",
+  studies: "Studies",
+  totalAnswers: "TotalAnswers",
+  percentageOfTotal: "PercentageOfTotal",
 };
 
 // Animations
